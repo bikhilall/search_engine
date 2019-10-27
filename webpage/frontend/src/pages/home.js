2aa7d6b9078f329f0df4ac28ae93a376ffbd1ee3
Amin <amin.mirakhorly@gmail.com>
2019-10-26 22:01:33 -0700

    add backend and docker

--- webpage/frontend/src/pages/home.js
+++ webpage/frontend/src/pages/home.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import { Container, Button } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 
 
+class SearchField extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  postValue(value) {
+    axios.post(process.env.REACT_APP_API_URL)
+      .then(res => {
+        console.log(res)
+      }
+      )
+  }
+
+  handleChange(event) {
+    this.postValue(event.target.value)
+  }
+
+  render() {
+    return (
+      <TextField
+        id="outlined-full-width"
+        onChange={(e) => { this.handleChange(e) }}
+        label="Search"
+        fullWidth
+        margin="normal"
+        variant="outlined"
+      />
+    )
+  }
+}
+
 class Home extends Component {
   render() {
     return (
       <Container>
-        <TextField
-          id="outlined-full-width"
-          label="Search"
-          fullWidth
-          margin="normal"
-          variant="outlined"
-        />
+        <SearchField />
       </Container>
     );
   }
