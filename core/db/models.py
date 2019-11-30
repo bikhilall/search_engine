8d062f82dc3b72c7ae934371e235eebf419fb2b3
amin <amin.mirakhorly@gmail.com>
2019-11-29 17:07:00 -0800

    db

--- core/db/models.py
+++ core/db/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, JSON, INT
 from .base import Base
 
 class Pages(Base):
     __tablename__ = 'pages'
-    vector = None
+    vector = Column(JSON())
     page_url = Column(String(50))
     language = Column(String(50))
