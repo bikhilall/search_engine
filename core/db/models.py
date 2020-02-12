d60a5310f1683c934a8e58c63818616435aa7680
amin <amin.mirakhorly@gmail.com>
2020-02-11 21:48:57 -0800

    db models

--- core/db/models.py
+++ core/db/models.py
@@ -1,9 +1,19 @@
-from sqlalchemy import Column, String, JSON, Integer
+from sqlalchemy import Column, String, JSON, Integer, DateTime
+from sqlalchemy.sql import func
 from .base import Base
+
+
+class Domaines(Base):
+    __tablename__ = 'domains'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(100))
+
 
 class Pages(Base):
     __tablename__ = 'pages'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    vector = Column(JSON())
-    url = Column(String(50))
+    url = Column(String(1000))
     language = Column(String(50))
+    vector = Column(JSON())
+    title = Column(String(100))
+    update_datetime = Column(DateTime(timezone=True), default=func.now())
