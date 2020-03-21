f2eb7a876cfffb00e33fb8660204f7a60bb2e588
amin <amin.mirakhorly@gmail.com>
2020-03-20 19:45:17 -0700

    initiate Gusel just once.

--- sentence_encoder/api/controllers/default_controller.py
+++ sentence_encoder/api/controllers/default_controller.py
@@ -1,9 +1,9 @@
 from api.models.text_vector import TextVector  # noqa: E501
 from encoders import Gusel
 
+encoder = Gusel()
 
 def encode(body):
-    encoder = Gusel()
     results = encoder.encode(body)
     text_vectors = [TextVector(text=r['text'], vector=r['vector']) for r in results]
 
